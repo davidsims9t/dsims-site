7a9ff74cc2901df871f822085be6122cbb14de4f
David Sims <dsims37@gatech.edu>
2019-03-20 22:28:01 -0700

    Fixed mobile grid styling and contact form ajax

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,19 +1,14 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+exports.onInitialClientRender = () => {
+    $("#contact-form").submit(function(e) {
+        e.preventDefault();
 
-// You can delete this file if you're not using it
-$("#contact-form").submit(function(e) {
-    e.preventDefault();
+        const $form = $(this);
+        $.post($form.attr("action"), $form.serialize()).then(function() {
+            $("#notice").css("display", "block");
 
-    const $form = $(this);
-    $.post($form.attr("action"), $form.serialize()).then(function() {
-        $("#notice").css("display", "block");
-
-        setTimeout(function() {
-            $("#notice").css("display", "none");
-        }, 1000);
+            setTimeout(function() {
+                $("#notice").css("display", "none");
+            }, 1000);
+        });
     });
-});
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ const Layout = ({ children }) => (
           {children}
         </main>
         <Footer />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
       </>
     )}
   />
